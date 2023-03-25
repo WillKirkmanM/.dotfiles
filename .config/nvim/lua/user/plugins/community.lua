307c9b735e1b521c480f3683ba7b8ca80838521c
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-03-25 20:22:49 +0000

    Feat(nvim): Added Mandatory Packs

---
+++ .config/nvim/lua/user/plugins/community.lua
@@ -0,0 +1,38 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of imporing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.kanagawa" },
+
+  -- Packs
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+
+  -- Motion
+  { import = "astrocommunity.motion.leap-nvim" },
+
+  -- Debugging
+  { import = "astrocommunity.debugging.nvim-bqf" },
+
+  -- Markdown and Latex
+  { import = "astrocommunity.markdown-and-latex.glow-nvim" },
+
+  -- Code Runner
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+
+  -- Editing Support
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.dial-nvim" },
+  -- { import = "astrocommunity.editing-support.nvim-ts-rainbow" },
+}
