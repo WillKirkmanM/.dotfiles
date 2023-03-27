fffd15b1c3ef2c6fb837d3bc4c0e8c1ab2c4945d
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-03-27 10:42:13 +0000

    Feat(nvim): Ensure Install Bash Syntax Highlighting

--- .config/nvim/lua/user/plugins/treesitter.lua
+++ .config/nvim/lua/user/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua", "javascript", "typescript", "rust", "cpp", "go", "html" },
+    ensure_installed = { "lua", "javascript", "typescript", "rust", "cpp", "go", "html", "bash" },
   },
 }
