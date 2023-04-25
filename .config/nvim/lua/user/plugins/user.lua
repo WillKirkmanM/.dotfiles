530ea26ff18d009cdd5bfed197a7436df5706331
WillKirkmanM <willkirkmanmoller@gmail.com>
2023-04-25 17:37:17 +0100

    Feat(nvim/user.lua): Added Vim Navigator

--- .config/nvim/lua/user/plugins/user.lua
+++ .config/nvim/lua/user/plugins/user.lua
@@ -1,4 +1,8 @@
 return {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false
+  }
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
